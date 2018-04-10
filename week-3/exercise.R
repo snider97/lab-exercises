c3656db91aeccc3a8cdc65fa9cec1f7319ed13c4
Student User <studentuser@D-173-250-197-166.dhcp4.washington.edu>
2018-04-10 14:24:52 -0700

    Fixed solution file

--- week-3/exercise.R
+++ week-3/exercise.R
@@ -3,21 +3,21 @@
 # What could be an issue here?
 event_capacity <- c(60, 50, '100', 100)
 
-# Fix the possible error in a new variable event_attendance_new
+# Fix the possible error in a new variable event_capacity_new
 event_capacity_new <- c(60, 50, 100, 100)
 
 # Create a vector of event names for each event 
 event_names <- c("Social Night", "Donut Fundraiser", "Career Night", "DIY Food")
 
 # Fix the error in creating this dataframe
-events <- data.frame(event_names, event_cost, event_profit, event_capacity)
+events <- data.frame(event_names, event_capacity)
 
 # Check the event_capacity column
 
 events$event_capacity
 
 # Try with event_capacity_new column
-events <- data.frame(event_names, event_cost, event_profit, event_capacity_new)
+events <- data.frame(event_names, event_capacity_new)
 
 # Are there any differences between event_capacity and event_capacity)_new?
 # There are levels for the column
